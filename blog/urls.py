306cf17690d94913b1dbb85f15b47a6ed8410e50
encima <encima@gmail.com>
2014-09-19 15:41:28 +0100

    added markdown support

--- blog/urls.py
+++ blog/urls.py
@@ -5,6 +5,6 @@ from blog import views
 urlpatterns = patterns('',
     url(r'^$', views.index, name='index'),
     url(r'^new/', views.new_post, name='new_blog_post'),
-    url(r'^view/(?P<slug>[^\.]+).html', views.view_post, name='view_blog_post'),
-    url(r'^tag/(?P<slug>[^\.]+).html', views.view_tag, name='view_blog_category'),
+    url(r'^view/(?P<slug>[^\.]+)', views.view_post, name='view_blog_post'),
+    url(r'^tag/(?P<slug>[^\.]+)', views.view_tag, name='view_blog_category'),
 )
--- blog/views.py
+++ blog/views.py
@@ -1,7 +1,8 @@
 from blog.models import Post, Tag, PostForm
 from django.shortcuts import render_to_response, get_object_or_404, render
 from django.http import HttpResponseRedirect
-import datetime
+import datetime, urllib
+import markdown
 
 def index(request):
     return render_to_response('index.html', {
@@ -13,20 +14,23 @@ def new_post(request):
     if(request.method == 'POST'):
         form = PostForm(request.POST)
         new = form.save(commit=False)
+        new.slug = urllib.quote_plus(new.title)
         new.pub_date = datetime.datetime.now() 
         t = Tag(1, 'test')
         t.save()
         new.tag = t
         new.save()
         print new
-        return HttpResponseRedirect('/blog')
+        return HttpResponseRedirect('/blog/view/' + new.slug)
     else:
         return render(request, 'new_post.html', {'form': PostForm(), 'title': 'New Post'})
 
 def view_post(request, slug):   
     print 'view post'
+    post = get_object_or_404(Post, slug=slug)
     return render_to_response('view_post.html', {
-        'post': get_object_or_404(Post, slug=slug)
+        'post': post,
+        'formatted_body': markdown.markdown(post.body) 
     })
 
 def save_post(request, slug):
